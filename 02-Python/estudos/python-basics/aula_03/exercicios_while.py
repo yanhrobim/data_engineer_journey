a31b2ad3ba93ed5d69215280ef5aa05f95b4a2de
Yan <yanrobim@gmail.com>
2026-05-15 15:58:46 -0300

    Python: Dia 13 (Exercicios WHILE)

---
+++ 02-Python/estudos/python-basics/aula_03/exercicios_while.py
@@ -0,0 +1,29 @@
+# ==========================================
+#     EXERCICÍCIOS CONTROLES DE FLUXO
+# ==========================================
+
+# ======================
+#        WHILE
+# ======================
+
+### Exercício 11. Leitura de Dados até Flag
+# Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
+
+palavra_chave = ''
+
+while palavra_chave != 'sair':
+    print("Digite 'sair' para sair do loop...")
+    palavra_chave = input("Digite se quer continuar no loop ou sair! ")
+print("Você saiu do loop!")
+
+### Exercício 12. Validação de Entrada
+# Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
+
+### Exercício 13. Consumo de API Simulado
+# Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
+
+### Exercício 14. Tentativas de Conexão
+# Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
+
+### Exercício 15. Processamento de Dados com Condição de Parada
+# Processar itens de uma lista até encontrar um valor específico que indica a parada.
