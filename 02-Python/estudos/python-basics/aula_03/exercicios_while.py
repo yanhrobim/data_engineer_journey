d5da1c9accdbf9b021c4882ca3bcf2db8aba7c3e
Yan <yanrobim@gmail.com>
2026-05-15 17:37:00 -0300

    Python: Dia 13 (Exercicios WHILE)

--- 02-Python/estudos/python-basics/aula_03/exercicios_while.py
+++ 02-Python/estudos/python-basics/aula_03/exercicios_while.py
@@ -14,6 +14,9 @@ palavra_chave = ''
 while palavra_chave != 'sair':
     print("Digite 'sair' para sair do loop...")
     palavra_chave = input("Digite se quer continuar no loop ou sair! ")
+    if not palavra_chave == 'sair':
+        palavra_chave = input("Digite se quer continuar no loop ou sair! ")
+
 print("Você saiu do loop!")
 
 
@@ -60,3 +63,25 @@ print("Reconexão estabelecida!")
 
 ### Exercício 5. Processamento de Dados com Condição de Parada
 # Processar itens de uma lista até encontrar um valor específico que indica a parada.
+
+# Uma lista com valores.
+# FOR para iterar os valores da lista.
+# Até WHILE achar valor especifico, roda o bloco, se achar break.
+
+lista_de_codigos = ["200", "301", "302", "307", "429", "500"] # Códigos HTTP que são tipo WARNING, aqueles que recebemos
+# mas não damos atenção no ínicio até dar problema :)
+codigo = 0
+while codigo != "500":
+    print("WARNING!!!")
+    if lista_de_codigos[codigo] == '500':      # Se locomovemos pela lista para o próximo valor através de 'codigo',
+                                               # onde 'codigo' é igual a 0 no ínicio e se adiciona +1 a cada volta do loop.
+                                               # A váriavel código funciona como index da lista, até encontrar o index
+                                               # igual a 500, ele não para.
+                                               # A iteração ocorre através de uma simulação de index com uma váriavel que
+                                               # de valor númerico que se adiciona +1 através do loop.
+                                               # Seria por exemplo eu armazenar em uma váriavel o valor de
+                                               # lista_de_codigos[5] = "500".
+        print("ERROR!!! PARADA ENCONTRADA!")
+        break
+    print(f"Resposta do Servidor: {lista_de_codigos[codigo]}")
+    codigo += 1
