6da786871b4239987b7ce875bb660a175069e565
Yan <yanrobim@gmail.com>
2026-05-15 16:09:14 -0300

    Python: Dia 13 (Exercicios WHILE)

--- 02-Python/estudos/python-basics/aula_03/exercicios_while.py
+++ 02-Python/estudos/python-basics/aula_03/exercicios_while.py
@@ -9,15 +9,23 @@
 ### Exercício 11. Leitura de Dados até Flag
 # Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
 
-palavra_chave = ''
+# palavra_chave = ''
 
-while palavra_chave != 'sair':
-    print("Digite 'sair' para sair do loop...")
-    palavra_chave = input("Digite se quer continuar no loop ou sair! ")
-print("Você saiu do loop!")
+# while palavra_chave != 'sair':
+#     print("Digite 'sair' para sair do loop...")
+#     palavra_chave = input("Digite se quer continuar no loop ou sair! ")
+# print("Você saiu do loop!")
 
 ### Exercício 12. Validação de Entrada
 # Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
+
+numero_user = int(input("Digite um número de 1 a 10: "))
+
+while numero_user < 1 or numero_user > 10:
+    print("Você digitou um número que não corresponde com o intervalo de 1 a 10!")
+    numero_user = int(input("Digite novamente um número de 1 a 10: "))
+print(f"Você digitou: {numero_user}. O número está no intervalo de 1 a 10.")
+
 
 ### Exercício 13. Consumo de API Simulado
 # Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
