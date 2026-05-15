c8924c9fa1a723ce4dc7abf2e81f84417dc68fea
Yan <yanrobim@gmail.com>
2026-05-15 16:42:43 -0300

    Python: Dia 13 (Exercicios WHILE)

--- 02-Python/estudos/python-basics/aula_03/exercicios_while.py
+++ 02-Python/estudos/python-basics/aula_03/exercicios_while.py
@@ -16,6 +16,7 @@ while palavra_chave != 'sair':
     palavra_chave = input("Digite se quer continuar no loop ou sair! ")
 print("Você saiu do loop!")
 
+
 ### Exercício 2. Validação de Entrada
 # Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
 
@@ -41,6 +42,21 @@ print("Todas as páginas foram acessadas!")
 
 ### Exercício 4. Tentativas de Conexão
 # Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
+
+import random
+
+reconexão = random.randint(1, 10)
+numero_user_reconexao = int(input("Ops! Você caiu! Digite um número de 0 a 10 para continuar, e se acertar o número você reconecta: "))
+tentativas = 5
+while numero_user_reconexao != reconexão:
+    print("Você não acertou! E consequentemente perdeu uma tentativa")
+    numero_user_reconexao = int(input(f"Você tem o total de {tentativas} tentativas para tentar reconectar! Digite mais uma vez: "))
+    tentativas -= 1
+    if tentativas == 0:
+        print("Você perdeu todas as tentativas :( ")
+        break
+print("Reconexão estabelecida!")
+
 
 ### Exercício 5. Processamento de Dados com Condição de Parada
 # Processar itens de uma lista até encontrar um valor específico que indica a parada.
