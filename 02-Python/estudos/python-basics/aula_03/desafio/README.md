43733c7001a30a44ddfaff6b9a96858d641ee995
Yan <yanrobim@gmail.com>
2026-05-16 13:28:35 -0300

    Python: Dia 14

---
+++ 02-Python/estudos/python-basics/aula_03/desafio/README.md
@@ -0,0 +1,5 @@
+### Desafio - Integre no projeto anterior um fluxo de While que repita o fluxo até que o usuário insira as informações corretas
+
+No projeto anterior desenvolvemos tratamento de erros caso houvesse bugs ou erros no software de *Calcúlo de Bônus 2024*, com try-except pensamos em possíveis bugs e criamos caminhos de forma que não ocorresse o travamento do código.
+
+O objetivo do projeto de hoje é potencializar estes tratamentos de erros, afim de fazer com que o usuário tenha mais tentativas caso viole alguma regra do software. Para fazer isso, vamos adicionar o **While** no código e fazer com que o loop não acabe até que valor seja correto.
---
+++ 02-Python/estudos/python-basics/aula_03/desafio/desafio_kpi_sem_bugs.py
@@ -0,0 +1,60 @@
+# =======================================================================================================================
+#   Integre no projeto anterior um fluxo de While que repita o fluxo até que o usuário insira as informações corretas
+# =======================================================================================================================
+
+try:
+    while True:             # Condição verdadeira, "loop infinito".
+        try:
+
+            nome_usuario = str(input("Digite seu nome: ")).strip().replace(" ", "")
+
+            if nome_usuario.isalpha():      # Condição que impede que o nome seja algum valor númerico. Ex: 10 
+                                                    # O comando isalpha() garante que na resposta (string) contenha somente respostas com valores alfabéticos, retornando True para aqueles valores que só possuem letras e False para valores que possuem números, espaços ou caracteres especiais.
+                                                    # Se o usuário colocar valores númericos como nome, irá ter o retorno de erros.
+                pass
+            else:
+                raise ValueError("O valor passado está incorreto! Escreva um valor tipo string! (De preferência um nome)")
+            
+            if len(nome_usuario) < 3:
+                raise ValueError("Nome incorreto! O nome inserido possuí menos de 3 carácteres, nome inválido!")
+            
+
+        except ValueError as e:
+            print(e)
+
+        else:
+            print("Nome verificado e correto!")
+            break       # Para quebrar o loop infinito, quando não ocorrer nenhuma exeção no try, o else é rodado com um 'break' que trava o loop.
+                        # Lógicamente, se chegar neste else, significa que o valor está correto.
+
+    try:
+        salario_usuario = input("Digite seu salário: ") # não pode ser negativo. não pode ser vázio. não pode ser string
+
+        if float(salario_usuario) < 0 or float(salario_usuario) == 0:
+            raise ValueError("O valor do salário não pode ser negativo ou zero! Tente novamente!")
+        else:
+            pass
+
+    except ValueError as e:
+        print(e)
+
+
+    try:
+        bonus_usuario = input("Digite seu bônus: ")
+
+        if float(bonus_usuario) < 0 or float(bonus_usuario) == 0:
+            raise ValueError("O valor do bônus não poder ser igual ou menor que 0! Não foi possível fazer o bônus 2024 ;( Tente novamente!")
+            
+        else:
+            pass
+
+
+    except ValueError as e:
+        print(e)
+        
+
+except Exception as e:
+    print(f"Ocorreu uma execeção! Detalhes da parte do erro: {e}")
+else:
+    bonus_2024 = 1000 + float(salario_usuario) * float(bonus_usuario)
+    print(f"Olá {nome_usuario}, o seu valor bônus de 2024 é: {bonus_2024}")
