8274760e6c224c24b04bfefda81958acdb57f81a
Yan <yanrobim@gmail.com>
2026-05-21 14:38:47 -0300

    Python: Dia 16 (Exercícios Lista)

---
+++ 02-Python/estudos/python-basics/aula_04/exercicios_list_e_dict.py
@@ -0,0 +1,18 @@
+# ================
+#       LIST
+# ================
+
+
+# 1. Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
+
+
+# 2. Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
+
+
+# 3. Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
+
+
+# 4. Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
+
+
+# 5. Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
