4353e2b8d956c1cab441ed7b6001ffecd886a396
Yan <yanrobim@gmail.com>
2026-05-29 15:18:35 -0300

    Python: Dia 20 (Funções Exemplos)

--- 02-Python/estudos/python-basics/Aula 5 (Funções e Estrutura de Dados)/funções/build-in_function.py
+++ 02-Python/estudos/python-basics/Aula 5 (Funções e Estrutura de Dados)/funções/build-in_function.py
@@ -35,3 +35,46 @@ media = soma_vendas / qtd_meses
 # A função round() em Python arredonda números decimais (float) para o valor inteiro mais próximo ou para um número específico de casas decimais.
 
 media = round(media, 2)
+
+
+# ==================
+#      FUNCÃO
+# ==================
+# Funções são uma sequência de instruções que criamos em um bloco de código para executar tarefas específicas.
+# Além disso, podem ser reutilzidas em diferentes partes do nosso código.
+
+# 1. Função Sem Parâmetros.
+
+def contar_caractere():
+    quantidade_caractere = len("palavra")
+    print(quantidade_caractere)
+
+contar_caractere()
+
+# 2. Função Com Parâmetros.
+
+def contar_caractere_parâmetro(palavra):
+    quantidade_caractere = len(palavra)
+    print(quantidade_caractere)
+
+contar_caractere_parâmetro(palavra="engenharia")
+
+# 3. Função Com Retorno.
+# Tanto funções com parâmetros ou sem, trabalham com 'escopo de função', onde significa que tuda variável
+# que foi criada, desenvolvida dentro da função, após a execução do bloco de código a variável morre.
+# Isso pode atrapalhar pois funções sem retorno retornam 'NoneType', e provavelmente você irá precisar do resultado que foi obtido dentro da função.
+
+
+def contar_caractere_parâmetro_return(palavra):
+    quantidade_caractere = len(palavra)
+
+    if quantidade_caractere >= 12:
+        situacao = "Palavra Consideravelmente Grande Para o Cotidiano"
+    
+    else:
+        situacao = "Palavra Normal Para o Cotidiano"
+
+    return (quantidade_caractere, situacao)
+
+qtd_caractere, situacao = contar_caractere_parâmetro_return(palavra="paralelepípedo")
+print(f"Você digitou uma palavra com {qtd_caractere} caracteres. Ela é uma {situacao}")
