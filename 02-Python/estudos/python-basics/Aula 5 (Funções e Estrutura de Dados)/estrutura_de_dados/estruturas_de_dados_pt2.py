1ce030e0bdad89ac0acb2a4bc1418e866fdbbaf5
Yan <yanrobim@gmail.com>
2026-06-02 15:28:19 -0300

    Python: Dia 23 (Tuplas)

--- 02-Python/estudos/python-basics/Aula 5 (Funções e Estrutura de Dados)/estrutura_de_dados/estruturas_de_dados_pt2.py
+++ 02-Python/estudos/python-basics/Aula 5 (Funções e Estrutura de Dados)/estrutura_de_dados/estruturas_de_dados_pt2.py
@@ -31,7 +31,6 @@ for i in range(len(historico_precos)):    # Individualizamos cada elemento atrav
     else:
         produto_precos_juntos.append(historico_precos[i])
 
-print(nome_produto)
 produto_precos = []
 
 for preco in range(0, len(produto_precos_juntos), 3):
@@ -39,10 +38,23 @@ for preco in range(0, len(produto_precos_juntos), 3):
 # O range está começando do index 0, e tem mais um parâmetro chamado 'step' quee está como 3. O step é responsável pela ordem no qual o valor é passado ao loop for.
 # Então, a cada rodada, o loop for recebe o index de 3 em 3, o primeiro index é 0, depois 3, depois 6, assim por adiante.
 # Quando executamos preco + 1 e preco + 2, estamaos apenas adicionamos a uma lista os valores após o index passado, novamente explicando, de 3 em 3.
-print(produto_precos)
 
 
 # Observação sobre lista de listas.
 # Caso queira pegar especifico valor em uma lista de lista:
 
 print(produto_precos[1][0]) # Em termos de hirárquia, primeiro vem o index da lista que desejamos o valor, depois o valor que queremos.
+
+produtos = []
+
+for nome_p, preco_p in zip(nome_produto, produto_precos):
+    produtos.append([nome_p, preco_p])
+print(produtos)
+
+# Resposta:
+# [['Sorvete', [20.0, 21.0, 25.0]], ['Arroz', [18.0, 22.0, 27.0]], ['Feijão', [9.0, 11.0, 14.0]], ['Leite', [5.0, 6.0, 7.5]], ['Pão', [8.0, 9.5, 12.0]]].
+
+# =================
+#      TUPLAS
+# =================
+
