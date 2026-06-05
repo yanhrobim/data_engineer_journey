4672fff64127639f22cb7dd4edb2cbdad1156f5a
Yan <yanrobim@gmail.com>
2026-06-05 14:03:38 -0300

    Python para Dados: Dia 1 (Leitura e Escrita de Arquivos)

--- 02-Python/estudos/python-to-data/Capítulo 1 (Leitura e Escrita de Arquivos em Python)/em_python/leitura_e_escrita_arquivos_python.py
+++ 02-Python/estudos/python-to-data/Capítulo 1 (Leitura e Escrita de Arquivos em Python)/em_python/leitura_e_escrita_arquivos_python.py
@@ -22,6 +22,7 @@ escrita = open("./dados_para_pratica/clientes.txt", "w", encoding="utf-8")
     # No segundo parâmetro pode ser "w" (write) que sobscrevre toda vez o arquivo com os dados novos ou
     # "a"(append) que adiciona os novos dados no arquivo.
     # Também existe a opção sem 'with' como feito acima, adicionando a função de abertura do arquivo a um parâmetro.
+    # O nome e caminho do arquivo é definido no primeiro parâmetro.
 
 for cliente in clientes:    # É preciso de um loop for para indivualizar os elementos, caso contrário, tudo é escrito na mesma linha.
     escrita.write(str(cliente) + "\n")  # É preciso transformar em string, para o write() aceitar, caso contrário, gera erro.
@@ -44,4 +45,31 @@ leitura_arquivo = leitura.read()
 # {'id': 8, 'nome': 'Lucas Teixeira', 'cidade': 'Manaus', 'compras': 6}
 # {'id': 9, 'nome': 'Camila Nunes', 'cidade': 'Brasília', 'compras': 9}
 # {'id': 10, 'nome': 'Diego Ferreira', 'cidade': 'Goiânia', 'compras': 2}
+
+
+# ===========================
+#    TIPO DE ARQUIVO CSV
+# ============================
+# Para a leitura ou escrita de arquivos '.csv', é necessário a importação do módulo 'csv' disponibilizado pelo próprio Python.
+
+# 1. Escrita de arquivos '.csv':
+import csv
+
+escrita_csv = open("./dados_para_pratica/clientes.csv", 'w', encoding="utf-8")
+
+colunas = ['id', 'nome', 'cidade', 'compras']   # O tipo de arquivo '.csv' possuí cabeçalho, que representa a coluna de cada dado.
+                                                # Basicamente, significa as chaves de um dict, e aqui coloquei na ordem das chaves do dict 'clientes' para evitar dados em colunas erradas.
+
+csv_config = csv.DictWriter(escrita_csv, fieldnames=colunas)    
+# O módulo 'csv' proporciona dois métodos para a escrita sendo o '.DictWriter()' ou o '.writer()'.
+# '.DictWriter()' Espera receber um iterável do tipo dicionário.
+# 'writer()' Espera receber um iterável do tipo lista.
+
+csv_config.writeheader()        # Aqui dizemos ao código que queremos escrever o cabeçalho.
+                                # Caso contrário, somente os dados são escritos dentro do arquivo, sem colunas para referencia-los.
+
+for cliente in clientes:
+    csv_config.writerow(cliente)    # Após individualizarmos cada elemento, escrevemos com 'writerow()'.
+
+
 
