933c52cabeca40bf3e0712ded0ba67e93cb09ad9
Yan <yanrobim@gmail.com>
2026-06-05 14:16:11 -0300

    Python para Dados: Dia 1 (Leitura e Escrita de Arquivos)

--- 02-Python/estudos/python-to-data/Capítulo 1 (Leitura e Escrita de Arquivos em Python)/em_python/leitura_e_escrita_arquivos_python.py
+++ 02-Python/estudos/python-to-data/Capítulo 1 (Leitura e Escrita de Arquivos em Python)/em_python/leitura_e_escrita_arquivos_python.py
@@ -17,22 +17,26 @@ clientes = [
 #    ESCRITA DE ARQUIVOS UTILIZANDO PYTHON
 # ===========================================
 
-escrita = open("./dados_para_pratica/clientes.txt", "w", encoding="utf-8")       
+with open("./dados_para_pratica/clientes.txt", "w", encoding="utf-8") as escrita:
     # with open é como se fosse uma função que abre e fecha automaticamente.
     # No segundo parâmetro pode ser "w" (write) que sobscrevre toda vez o arquivo com os dados novos ou
     # "a"(append) que adiciona os novos dados no arquivo.
-    # Também existe a opção sem 'with' como feito acima, adicionando a função de abertura do arquivo a um parâmetro.
     # O nome e caminho do arquivo é definido no primeiro parâmetro.
 
-for cliente in clientes:    # É preciso de um loop for para indivualizar os elementos, caso contrário, tudo é escrito na mesma linha.
-    escrita.write(str(cliente) + "\n")  # É preciso transformar em string, para o write() aceitar, caso contrário, gera erro.
+    for cliente in clientes:    # É preciso de um loop for para indivualizar os elementos, caso contrário, tudo é escrito na mesma linha.
+        escrita.write(str(cliente) + "\n")  # É preciso transformar em string, para o write() aceitar, caso contrário, gera erro.
     
+# Também existe a opção sem 'with' como feito acima, adicionando a função de abertura do arquivo a um parâmetro.
+# Porém, é necessário adicionar ao código '.close()' para sinalizar quando fechar.
+
+
 # ============================================
 #    LEITURA DE ARQUIVOS UTILIZANDO PYTHON
 # ============================================
 
-leitura = open("./dados_para_pratica/clientes.txt", "r", encoding="utf-8") # Invés de 'w', aqui utilizamos 'r'(read) para ler o arquivo.
-leitura_arquivo = leitura.read()
+with open("./dados_para_pratica/clientes.txt", "r", encoding="utf-8") as leitura: # Invés de 'w', aqui utilizamos 'r'(read) para ler o arquivo.
+    leitura_arquivo = leitura.read()
+print(leitura_arquivo)
 
 # Resposta:
 # {'id': 1, 'nome': 'Ana Lima', 'cidade': 'São Paulo', 'compras': 5}
@@ -47,29 +51,41 @@ leitura_arquivo = leitura.read()
 # {'id': 10, 'nome': 'Diego Ferreira', 'cidade': 'Goiânia', 'compras': 2}
 
 
-# ===========================
-#    TIPO DE ARQUIVO CSV
-# ============================
+# ======================================================
+#    TIPO DE ARQUIVO CSV (UTILIZANDO SOMENTE PYTHON)
+# ======================================================
 # Para a leitura ou escrita de arquivos '.csv', é necessário a importação do módulo 'csv' disponibilizado pelo próprio Python.
 
 # 1. Escrita de arquivos '.csv':
+
 import csv
 
-escrita_csv = open("./dados_para_pratica/clientes.csv", 'w', encoding="utf-8")
+with open("./dados_para_pratica/clientes.csv", 'w', encoding="utf-8") as escrita_csv:
 
-colunas = ['id', 'nome', 'cidade', 'compras']   # O tipo de arquivo '.csv' possuí cabeçalho, que representa a coluna de cada dado.
+    colunas = ['id', 'nome', 'cidade', 'compras']   # O tipo de arquivo '.csv' possuí cabeçalho, que representa a coluna de cada dado.
                                                 # Basicamente, significa as chaves de um dict, e aqui coloquei na ordem das chaves do dict 'clientes' para evitar dados em colunas erradas.
 
-csv_config = csv.DictWriter(escrita_csv, fieldnames=colunas)    
+    csv_config = csv.DictWriter(escrita_csv, fieldnames=colunas)    
 # O módulo 'csv' proporciona dois métodos para a escrita sendo o '.DictWriter()' ou o '.writer()'.
-# '.DictWriter()' Espera receber um iterável do tipo dicionário.
+# A diferença é que '.DictWriter()' Espera receber um iterável do tipo dicionário.
 # 'writer()' Espera receber um iterável do tipo lista.
 
-csv_config.writeheader()        # Aqui dizemos ao código que queremos escrever o cabeçalho.
+    csv_config.writeheader()        # Aqui dizemos ao código que queremos escrever o cabeçalho.
                                 # Caso contrário, somente os dados são escritos dentro do arquivo, sem colunas para referencia-los.
 
-for cliente in clientes:
-    csv_config.writerow(cliente)    # Após individualizarmos cada elemento, escrevemos com 'writerow()'.
+    for cliente in clientes:
+        csv_config.writerow(cliente)    # Após individualizarmos cada elemento, escrevemos com 'writerow()'.
 
+# 2. Leitura de arquivos '.csv':
 
+with open("./dados_para_pratica/clientes.csv", 'r', encoding="utf-8") as leitura:
+
+    leitura_csv = csv.DictReader(leitura)
+# O módulo 'csv' proporciona dois métodos para a leitura sendo o '.DictReader()' ou o '.reader()'.
+# A diferença é que '.DictReader()' retorna um dicionário, a váriavel que le o arquivo se torna um dict.
+# Sendo melhor para em processos futuros lidar com estes dados, tendo em vista que cada um contém sua chave, ao invés de uma sequência bagunçada.
+# E '.reader()' nos retorna uma lista, para pegar dados precisariamos do index. Depende do tipo de dado, porém os dois tem suas vantagens e desvantagens.
+
+    for linha in leitura_csv:
+        print(linha)
 
