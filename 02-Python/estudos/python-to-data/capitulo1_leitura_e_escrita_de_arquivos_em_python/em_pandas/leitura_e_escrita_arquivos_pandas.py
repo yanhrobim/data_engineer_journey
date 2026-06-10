c6f4c4c0cb3cee448c019c694337830f21e860c1
Yan <yanrobim@gmail.com>
2026-06-10 17:09:49 -0300

    Python Para Dados: Dia 4 (Explicação sobre DataFrame)

--- 02-Python/estudos/python-to-data/capitulo1_leitura_e_escrita_de_arquivos_em_python/em_pandas/leitura_e_escrita_arquivos_pandas.py
+++ 02-Python/estudos/python-to-data/capitulo1_leitura_e_escrita_de_arquivos_em_python/em_pandas/leitura_e_escrita_arquivos_pandas.py
@@ -20,13 +20,62 @@ clientes = [
 #    ESCRITA DE ARQUIVOS UTILIZANDO PANDAS (CSV)
 # =================================================
 
-escrever = pd.DataFrame(clientes)
-escrever.to_csv("./dados_para_pratica/clientes.csv")
+escrever = pd.DataFrame(clientes) # Para escrever arquivos com 'pandas' é necessário transformar seus dados em um DataFrame.
+
+escrever.to_csv("./dados_para_pratica/clientes.csv", index=False)   # Após a transformação, o objeto DataFrame nos proporciona métodos 
+                                                       # como este o 'to_csv()'.
 
 
 # ===============================================
 #    LEITURA ARQUIVOS UTILIZANDO PANDAS (CSV)
 # ===============================================
 
-leitura = pd.read_csv("./dados_para_pratica/clientes.csv")
-print(leitura)
+clientes_df = pd.read_csv("./dados_para_pratica/clientes.csv")
+
+
+# ===================================================
+#    ESTRUTURA DE DADOS BIODIMENSIONAL: DATAFRAME
+# ===================================================
+# Um DataFrame é uma estrutura de dados podendo ser criada com Pandas em Python. 
+# Ela é mais complexa que listas e dicionários, porém é muito semelhante a uma lista de dicionários, 
+# onde por trás dos panos se torna uma lista, que armazena dados que são separados por chaves e valores.
+# Porém, o DataFrame não é uma lista de dicionários, e sim uma tabela dentro de  Python, 
+# aonde é organizada por Colunas (Chaves), Linhas (O dict ao todo; Cada dado) e Index (Representa a posição desta linha na tabela).
+# (Por isso biodimensional)
+# A diferença entre uma lista de dicionários e um DataFrame é justamente esta organização e separação, 
+# conseguindo aplicar transformações em nível de linha, coluna e por index.
+
+
+# =====================================
+#    PRINCIPAIS MÉTODOS (LEITURA)
+# =====================================
+# Pedi ao Claude para me mandar os comandos mais útlizados no cotidiano de dados para lidar com DataFrame.
+# Seguindo a documentação, e em caso de dúvidas perguntando ao Claude, pratiquei e documentei com cada comando que ele me enviou.
+# Logicamente, pode não ser os comandos mais útilizados, errado ali ou aqui, mas o objetivo é ao menos entender o básico sobre os métodos do objeto DataFrame.
+
+clientes_df.head()  # .head() nos proporciona adicionar uma filtragem de linhas que queremos pegar do DataFrame.
+                    # caso passado nenhum número, nos retorna as 5 primeiras linhas como padrão.
+
+clientes_df.tail()  # Semelhante ao .head(), porém retorna as 5 últimas linhas caso não seja passado nenhum valor.
+
+print(clientes_df.shape) # O método .shape() nos retorna a quantidade de registros dentro do DataFrame e quantidade de colunas.
+
+# clientes_df.info() # O método .info() nos retorna informações mais específicas do nosso DataFrame, como dtypes de colunas, valores nulos entre outras informações.
+
+# RangeIndex: 10 entries, 0 to 9
+# Data columns (total 4 columns):
+#  #   Column   Non-Null Count  Dtype
+# ---  ------   --------------  -----
+#  0   id       10 non-null     int64
+#  1   nome     10 non-null     str  
+#  2   cidade   10 non-null     str  
+#  3   compras  10 non-null     int64
+# dtypes: int64(2), str(2)
+# memory usage: 452.0 bytes
+
+print(clientes_df['compras'].describe()) # O método describe() nos retorna estásticas sobre nossas colunas númericas como:
+                                         # Valor máximo, dtype da coluna, 25% (primeiro quartil), 75% (último quartil), entre outras.
+
+print(clientes_df.columns) # .colums() nos retorna o nome das colunas presentes no DataFrame.
+print(clientes_df.dtypes)  # .dtypes() nos retorna o tipo da coluna(s).
+
