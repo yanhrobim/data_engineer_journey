6dd3eda83b22dadc9f52d1bab4b426595bdc2d7c
Yan <yanrobim@gmail.com>
2026-06-13 13:37:19 -0300

    Python Para Dados: Dia 6 (Desafio: Calcúlo Valor Total)

--- 02-Python/estudos/python-to-data/capitulo1_leitura_e_escrita_de_arquivos_em_python/desafio/leitura_e_transformacao.py
+++ 02-Python/estudos/python-to-data/capitulo1_leitura_e_escrita_de_arquivos_em_python/desafio/leitura_e_transformacao.py
@@ -4,4 +4,7 @@ def leitura_dados_csv(caminho_para_dados_csv: str):
     dados_csv = pd.read_csv(filepath_or_buffer=caminho_para_dados_csv, sep=',')
     return dados_csv
 
+def calculo_valor_total(dados_csv, nome_coluna_de_quantidade: str, nome_coluna_de_preco_unitario: str):
+    return dados_csv[f'{nome_coluna_de_quantidade}'] * dados_csv[f'{nome_coluna_de_preco_unitario}']
+
 
