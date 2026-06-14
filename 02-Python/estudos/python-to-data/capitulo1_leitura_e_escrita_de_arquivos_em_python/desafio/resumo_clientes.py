ccdc098ffaf912df0ccf9dc6aa40096c5ecd7546
Yan <yanrobim@gmail.com>
2026-06-14 14:03:11 -0300

    Python Para Dados: Dia 7 (Desafio: Valor Total Gasto P/ Cliente)

---
+++ 02-Python/estudos/python-to-data/capitulo1_leitura_e_escrita_de_arquivos_em_python/desafio/resumo_clientes.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from leitura_e_transformacao import leitura_dados_csv, apply_valor_total
+
+leitura_csv = leitura_dados_csv("./dados_desafio/pedidos.csv")
+
+transformacao = apply_valor_total(dados_csv=leitura_csv,
+                                  nome_coluna_de_quantidade='quantidade',
+                                  nome_coluna_de_preco_unitario='preco_unitario')
+
+agrupando_p_cliente  = transformacao.groupby(by=['cliente'], group_keys=True, as_index=False).apply(lambda groupby: sum(groupby['valor_total']))
+print(pd.DataFrame(agrupando_p_cliente).rename(columns={None: "valor_total_gasto"}))
+
