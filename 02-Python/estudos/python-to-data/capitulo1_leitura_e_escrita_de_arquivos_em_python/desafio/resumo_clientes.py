c84e6eaf252962119b092bed4ac4ec3e67524552
Yan <yanrobim@gmail.com>
2026-06-14 14:29:41 -0300

    Python Para Dados: Dia 7 (Desafio: Função Para A Criação do JSON)

--- 02-Python/estudos/python-to-data/capitulo1_leitura_e_escrita_de_arquivos_em_python/desafio/resumo_clientes.py
+++ 02-Python/estudos/python-to-data/capitulo1_leitura_e_escrita_de_arquivos_em_python/desafio/resumo_clientes.py
@@ -1,13 +1,8 @@
 import pandas as pd
-from leitura_e_transformacao import leitura_dados_csv, apply_valor_total
 
-leitura_csv = leitura_dados_csv("./dados_desafio/pedidos.csv")
+def valor_total_gasto_p_cliente_in_json(dataframe_com_valor_total: pd.DataFrame, path_para_json: str):
 
-transformacao = apply_valor_total(dados_csv=leitura_csv,
-                                  nome_coluna_de_quantidade='quantidade',
-                                  nome_coluna_de_preco_unitario='preco_unitario')
-
-agrupando_p_cliente  = transformacao.groupby(by=['cliente'], group_keys=True, as_index=False).apply(lambda groupby: sum(groupby['valor_total']))
-pd.DataFrame(agrupando_p_cliente).rename(columns={None: "valor_total_gasto"}).to_json(path_or_buf="./dados_desafio/resumo_clientes.json",
+    agrupando_p_cliente  = dataframe_com_valor_total.groupby(by=['cliente'], group_keys=True, as_index=False).apply(lambda groupby: sum(groupby['valor_total']))
+    pd.DataFrame(agrupando_p_cliente).rename(columns={None: "valor_total_gasto"}).to_json(path_or_buf=path_para_json,
                                                                                       orient='records', indent=4, force_ascii=False)
-
+    return print(f"O JSON foi criado com sucesso! Localizado em: '{path_para_json}'.")
