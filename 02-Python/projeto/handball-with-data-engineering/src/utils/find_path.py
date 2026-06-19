677dc27d88b429b5fdb36d1418591ed9ad3602b6
Yan <yanrobim@gmail.com>
2026-06-19 14:06:11 -0300

    Python: Projeto Prático (Path Para Encontrar arquivo.csv)

--- 02-Python/projeto/handball-with-data-engineering/src/utils/find_path.py
+++ 02-Python/projeto/handball-with-data-engineering/src/utils/find_path.py
@@ -2,13 +2,24 @@ from pathlib import Path
 
 def encontrar_caminho_dados_csv(pasta_com_dados:str, nome_dados: str):
 
-    raiz = Path(__file__).parent.parent.parent
+    try:
+        raiz = Path(__file__).parent.parent.parent
 
-    caminho_para_dados = raiz / pasta_com_dados / nome_dados
+        caminho_para_dados = raiz / pasta_com_dados / nome_dados
 
-    return caminho_para_dados
+        if caminho_para_dados.exists() == False:
+            raise FileNotFoundError
+                    
+    except FileNotFoundError:
+        print("O caminho que foi desenvolvido não existe! Verifique os parâmetros que você passou!")
+        print(f"Parâmetros passados: '{pasta_com_dados}'; '{nome_dados}'. \nCaminho Final: '{caminho_para_dados}'")
 
-caminho_path = encontrar_caminho_dados_csv(pasta_com_dados='data', nome_dados='bundeshandball_sujo.csv')
+    else:
+        print("O path para o arquivo.csv foi criado com sucesso! Todos os parâmetros passados encontram um arquivo.csv!")
+        print(f"Path criado: {caminho_para_dados}")
+        return caminho_para_dados
+
+caminho_path = encontrar_caminho_dados_csv(pasta_com_dados='dat', nome_dados='bundeshandball_sujo.csv')
 
 # Resposta Função:
 # /home/usuario/Projetos/data_engineer_journey/02-Python/projeto/handball-with-data-engineering/data/bundeshandball_sujo.csv
