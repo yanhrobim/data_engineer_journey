d16d0bde1e0c1d3f6033bdb61a3e73e99580f0bb
Yan <yanrobim@gmail.com>
2026-06-24 11:42:48 -0300

    Python: Projeto Prático (Função Para Limpar Valores Nulos)

--- 02-Python/projeto/handball-with-data-engineering/src/cleaning/limpeza_inconsistencias.py
+++ 02-Python/projeto/handball-with-data-engineering/src/cleaning/limpeza_inconsistencias.py
@@ -1,14 +1,12 @@
 import pandas as pd
-from pandas import Series
 from src.extract.extracao_leitura_dados import leitura_csv
-import numpy as np
 
 leitura =  leitura_csv(nome_pasta_com_arquivo_csv="data/raw", nome_arquivo="bundeshandball.csv")
 
 
-def lidar_com_colunas_null(budeshandball_csv: pd.DataFrame):
+def lidar_com_colunas_null(return_leitura_csv: pd.DataFrame):
 
-    colunas_com_null = budeshandball_csv.columns[budeshandball_csv.isnull().any()].tolist() 
+    colunas_com_null = return_leitura_csv.columns[return_leitura_csv.isnull().any()].tolist() 
     # O objetivo dessa linha é obter as colunas que possuem algum valor nulo. Faço isso utilizando alguns métodos do pandas: .columns, .isnull(),.any() e .tolist().
     # .isnull() retorna True para valores, registros, que possuem nulo. O .any() retorna como resultado tudo aquilo que é True, 
     # se fosse o filtro em linha, ele só retornaria a coluna e dado da linha que é nulo.
@@ -22,24 +20,13 @@ def lidar_com_colunas_null(budeshandball_csv: pd.DataFrame):
 
     for coluna in colunas_com_null:
 
-        if budeshandball_csv[coluna].dtype == float:
-            coluna_numerica_sem_null =  budeshandball_csv[coluna].fillna(0)
-            budeshandball_csv[coluna] = coluna_numerica_sem_null
+        if return_leitura_csv[coluna].dtype == float:
+            coluna_numerica_sem_null =  return_leitura_csv[coluna].fillna(0)
+            return_leitura_csv[coluna] = coluna_numerica_sem_null
 
-        print(budeshandball_csv[coluna].dtypes)
-
-        if budeshandball_csv[coluna].dtype == 'str': 
-            coluna_str_sem_null = budeshandball_csv[coluna].fillna("valor_faltante")
-            budeshandball_csv[coluna] = coluna_str_sem_null
+        if return_leitura_csv[coluna].dtype == 'str': 
+            coluna_str_sem_null = return_leitura_csv[coluna].fillna("valor_faltante")
+            return_leitura_csv[coluna] = coluna_str_sem_null
         
-    return budeshandball_csv
+    return return_leitura_csv
               
-
-
-
-
-
-
-# if str(types_colum[colunas_com_null]) == 'float64':
-#     leitura[colunas_com_null].fillna(value=0)
-#     print(leitura['Nome'])
