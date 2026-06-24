cea55d343f70e63c46021e14449342e89774138b
Yan <yanrobim@gmail.com>
2026-06-24 12:37:32 -0300

    Python: Projeto Prático (Função Para Padronizar Nome Colunas)

--- 02-Python/projeto/handball-with-data-engineering/src/cleaning/limpeza_inconsistencias.py
+++ 02-Python/projeto/handball-with-data-engineering/src/cleaning/limpeza_inconsistencias.py
@@ -3,6 +3,11 @@ from src.extract.extracao_leitura_dados import leitura_csv
 
 leitura =  leitura_csv(nome_pasta_com_arquivo_csv="data/raw", nome_arquivo="bundeshandball.csv")
 
+def padronizando_nome_colunas(return_leitura_csv: pd.DataFrame):
+    nome_minusculo = [str.lower(coluna) for coluna in return_leitura_csv.columns]
+    return_leitura_csv.columns = nome_minusculo
+    return leitura
+
 
 def lidar_com_colunas_null(return_leitura_csv: pd.DataFrame):
 
