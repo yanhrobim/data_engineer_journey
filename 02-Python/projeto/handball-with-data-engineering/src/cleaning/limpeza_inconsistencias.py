88ad56fbd310bd56255706cf3167e771b019684f
Yan <yanrobim@gmail.com>
2026-06-26 14:58:54 -0300

    Python: Projeto Prático (Função Para Formatar Numeros Com Decimais Gigantes Para Duas Casas Decimais)

--- 02-Python/projeto/handball-with-data-engineering/src/cleaning/limpeza_inconsistencias.py
+++ 02-Python/projeto/handball-with-data-engineering/src/cleaning/limpeza_inconsistencias.py
@@ -87,13 +87,13 @@ def lidando_com_valores_impossiveis_invalidos(return_leitura_csv: pd.DataFrame) 
 # Devolver o df.
 
 
-def padronizar_formato_season_para_ano_completo(return_leitura_csv: pd.DataFrame):
+def padronizar_formato_season_para_ano_completo(return_leitura_csv: pd.DataFrame, nome_coluna_season: str):
     # A coluna "Season" veio com a inconsistência de se ter mais de um formato de data dentro da coluna.
     # ('21/22', '22/23', '20/21', '18/19', '23/24', '19/20', '17/18', '2017-2018')  
     # Isso poderia atrapalhar em contrução de futuros filtros, ou até mesmo para obter dados estratégicos.
     # Tive a decisão de aplicar um padrão a cada data da coluna, sendo 2017/2018.
 
-    return_leitura_csv['season'] = return_leitura_csv['season'].str.replace(pat=r'(\d{2})\/(\d{2})', repl=r'20\1/20\2', regex=True)
+    return_leitura_csv[f'{nome_coluna_season}'] = return_leitura_csv[f'{nome_coluna_season}'].str.replace(pat=r'(\d{2})\/(\d{2})', repl=r'20\1/20\2', regex=True)
     # Utilizei os metodos 'str.replace()' do Pandas, o 'str' nos possibilita obter as operações de texto para um Dataframe e o
     # 'replace()' tem o objetivo de encontrar e substituir strings de acordo com o parâmetro passado a ele.
     # Além dos métodos, utilizei um padrão regex para auxiliar a encontrar o formato desejado.
@@ -104,10 +104,16 @@ def padronizar_formato_season_para_ano_completo(return_leitura_csv: pd.DataFrame
     # Com cada data do formato '17/18' em um grupo de captura, passo o novo formato que quero para o .replace() reescrever a cada string que ele encontrar seguindo o padrão regex.
             
 
-    return_leitura_csv['season'] = return_leitura_csv['season'].str.replace(pat="-", repl="/")
+    return_leitura_csv[f'{nome_coluna_season}'] = return_leitura_csv[f'{nome_coluna_season}'].str.replace(pat="-", repl="/")
     # No dataset, a coluna 'season' também possuí os dados '2017-2018', então se caso ele encontrar estes dados, apenas substituir o '-'
     # para colocar no padrão decidido.
 
     return return_leitura_csv
 
+
+def formatacao_duas_casas_decimais(return_leitura_csv: pd.DataFrame, coluna_decimal: str):
+
+    return_leitura_csv[f'{coluna_decimal}'] = (round(return_leitura_csv[f'{coluna_decimal}'], 2))
+    
+    return return_leitura_csv
 
