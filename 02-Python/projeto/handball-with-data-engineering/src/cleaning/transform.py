f6cc71fd6d47ef70e8641eaeef3483a260939b44
Yan <yanrobim@gmail.com>
2026-07-01 14:27:14 -0300

    Python: Projeto Prático (Desenvolvendo Etapa de Transformação)

---
+++ 02-Python/projeto/handball-with-data-engineering/src/cleaning/transform.py
@@ -0,0 +1,48 @@
+import pandera.pandas as pa
+import pandas as pd
+from src.validation.validacao_schema import RawSchemaBudesHandball
+from src.cleaning.limpeza import padronizando_nome_colunas, formatacao_duas_casas_decimais, removendo_linhas_duplicadas, lidar_com_colunas_null, padronizar_formato_season_para_ano_completo, remover_caracteres_especiais, lidando_com_valores_impossiveis_invalidos
+from src.extract.extracao_leitura_dados import leitura_csv
+
+leitura =  leitura_csv(nome_pasta_com_arquivo_csv="data/raw", nome_arquivo="bundeshandball.csv")
+
+
+try:
+    RawSchemaBudesHandball.validate(leitura, lazy=True)
+    # O parâmetro 'lazy=' do método validade() nos permite encontrar mais de um erro em apenas uma execução. 
+    # Resumidamente, mesmo que ele encontre o primeiro erro, 
+    # com 'lazy' ele continuara até encontrar todos os erros e no final te entregar tudo de uma única vez, invés de parar no
+    # primeiro erro encontrado.
+
+except pa.errors.SchemaErrors as exc:
+    print(exc)
+
+relatorios = []
+
+lista_funcoes_de_limpeza = [
+    (padronizando_nome_colunas, {}),
+
+    (formatacao_duas_casas_decimais, {"coluna_decimal": "shooting_accuracy"}),
+    # Se função, precisar mais do que o DataFrame como argumento, é preciso transforma-la aqui em uma tupla, dois valores.
+    # Na tupla, o primeiro valor é a função, e o segundo valor são os argumentos extras, aqueles que são necessários tirando o DataFrame.
+    # Então, se função precisa do argumento 'df' e 'coluna', no dict você somente passa a chave 'coluna' e o valor que é necessário.
+
+    (removendo_linhas_duplicadas, {}),
+
+    (lidar_com_colunas_null, {}),
+
+    (padronizar_formato_season_para_ano_completo, {"nome_coluna_season": "season"}),
+
+    (remover_caracteres_especiais, {}),
+
+    (lidando_com_valores_impossiveis_invalidos, {})
+]
+
+for funcao, parametros_extras_funcoes in lista_funcoes_de_limpeza:
+    leitura, relatorio = funcao(leitura, **parametros_extras_funcoes)
+    # Aqui, o famoso '**kwargs' tem o objetivo de descompactar o dicionário dentro da tupla, e transforma-los em parâmetros e valores.
+    # Basicamente, ele reescreve assim para o Python: 
+    # leitura, relatorio = formatacao_duas_casas_decimais(df, coluna_decimal="shooting_accuracy")
+
+    relatorios.append(relatorio)
+
