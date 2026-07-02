049a88652229a13ee6506e21428e1a0a54507b89
Yan <yanrobim@gmail.com>
2026-07-02 13:54:05 -0300

    Python: Projeto Prático (Função para a Transformação do DataFrame)

--- 02-Python/projeto/handball-with-data-engineering/src/cleaning/transform.py
+++ 02-Python/projeto/handball-with-data-engineering/src/cleaning/transform.py
@@ -1,48 +1,51 @@
 import pandera.pandas as pa
 import pandas as pd
-from src.validation.validacao_schema import RawSchemaBudesHandball
 from src.cleaning.limpeza import padronizando_nome_colunas, formatacao_duas_casas_decimais, removendo_linhas_duplicadas, lidar_com_colunas_null, padronizar_formato_season_para_ano_completo, remover_caracteres_especiais, lidando_com_valores_impossiveis_invalidos
-from src.extract.extracao_leitura_dados import leitura_csv
+from src.utils.utils import criar_relatorio_qualidade_de_dados_json
+from src.validation.validacao_schema import RawSchemaBudesHandball
 
-leitura =  leitura_csv(nome_pasta_com_arquivo_csv="data/raw", nome_arquivo="bundeshandball.csv")
+def transform_budeshandball_csv(budeshandball_dataframe: pd.DataFrame):
+    try:
+        RawSchemaBudesHandball.validate(budeshandball_dataframe, lazy=True)
+        # O parâmetro 'lazy=' do método validade() nos permite encontrar mais de um erro em apenas uma execução. 
+        # Resumidamente, mesmo que ele encontre o primeiro erro, 
+        # com 'lazy' ele continuara até encontrar todos os erros e no final te entregar tudo de uma única vez, invés de parar no
+        # primeiro erro encontrado.
 
+    except pa.errors.SchemaErrors as exc:
+        print(exc)
 
-try:
-    RawSchemaBudesHandball.validate(leitura, lazy=True)
-    # O parâmetro 'lazy=' do método validade() nos permite encontrar mais de um erro em apenas uma execução. 
-    # Resumidamente, mesmo que ele encontre o primeiro erro, 
-    # com 'lazy' ele continuara até encontrar todos os erros e no final te entregar tudo de uma única vez, invés de parar no
-    # primeiro erro encontrado.
+    relatorios = []
 
-except pa.errors.SchemaErrors as exc:
-    print(exc)
+    lista_funcoes_de_limpeza = [
+        (padronizando_nome_colunas, {}),
 
-relatorios = []
+        (formatacao_duas_casas_decimais, {"coluna_decimal": "shooting_accuracy"}),
+        # Se função, precisar mais do que o DataFrame como argumento, é preciso transforma-la aqui em uma tupla, dois valores.
+        # Na tupla, o primeiro valor é a função, e o segundo valor são os argumentos extras, aqueles que são necessários tirando o DataFrame.
+        # Então, se função precisa do argumento 'df' e 'coluna', no dict você somente passa a chave 'coluna' e o valor que é necessário.
 
-lista_funcoes_de_limpeza = [
-    (padronizando_nome_colunas, {}),
+        (removendo_linhas_duplicadas, {}),
 
-    (formatacao_duas_casas_decimais, {"coluna_decimal": "shooting_accuracy"}),
-    # Se função, precisar mais do que o DataFrame como argumento, é preciso transforma-la aqui em uma tupla, dois valores.
-    # Na tupla, o primeiro valor é a função, e o segundo valor são os argumentos extras, aqueles que são necessários tirando o DataFrame.
-    # Então, se função precisa do argumento 'df' e 'coluna', no dict você somente passa a chave 'coluna' e o valor que é necessário.
+        (lidar_com_colunas_null, {}),
 
-    (removendo_linhas_duplicadas, {}),
+        (padronizar_formato_season_para_ano_completo, {"nome_coluna_season": "season"}),
 
-    (lidar_com_colunas_null, {}),
+        (remover_caracteres_especiais, {}),
 
-    (padronizar_formato_season_para_ano_completo, {"nome_coluna_season": "season"}),
+        (lidando_com_valores_impossiveis_invalidos, {})
+    ]
 
-    (remover_caracteres_especiais, {}),
+    for funcao, parametros_extras_funcoes in lista_funcoes_de_limpeza:
+        budeshandball_dataframe, relatorio_func = funcao(budeshandball_dataframe, **parametros_extras_funcoes)
+        # Aqui, o famoso '**kwargs' tem o objetivo de descompactar o dicionário dentro da tupla, e transforma-los em parâmetros e valores.
+        # Basicamente, ele reescreve assim para o Python: 
+        # leitura, relatorio = formatacao_duas_casas_decimais(df, coluna_decimal="shooting_accuracy")
 
-    (lidando_com_valores_impossiveis_invalidos, {})
-]
+        relatorios.append(relatorio_func)
 
-for funcao, parametros_extras_funcoes in lista_funcoes_de_limpeza:
-    leitura, relatorio = funcao(leitura, **parametros_extras_funcoes)
-    # Aqui, o famoso '**kwargs' tem o objetivo de descompactar o dicionário dentro da tupla, e transforma-los em parâmetros e valores.
-    # Basicamente, ele reescreve assim para o Python: 
-    # leitura, relatorio = formatacao_duas_casas_decimais(df, coluna_decimal="shooting_accuracy")
+    criar_relatorio_qualidade_de_dados_json(relatorios=relatorios, 
+                                            pastas_onde_relatorio_deve_ser_salvo="data/reports/data_quality_report",
+                                            nome_para_relatorio="data_quality_report.json")
 
-    relatorios.append(relatorio)
-
+    return budeshandball_dataframe
