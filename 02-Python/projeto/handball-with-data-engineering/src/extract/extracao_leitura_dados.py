c8893240baa6499c334a0bc7e97aa64f9ecf35c0
Yan <yanrobim@gmail.com>
2026-07-09 11:53:41 -0300

    Python: Projeto Prático (Etapa de Leitura do Pipeline)

--- 02-Python/projeto/handball-with-data-engineering/src/extract/extracao_leitura_dados.py
+++ 02-Python/projeto/handball-with-data-engineering/src/extract/extracao_leitura_dados.py
@@ -3,18 +3,23 @@ import pandas.errors
 from src.utils.utils import encontrar_caminho_dados_csv
 
 def leitura_budeshandball_csv(nome_pasta_com_arquivo_csv: str, nome_arquivo: str) -> pd.DataFrame:
+
+    print("\n========================================")
+    print("  ETAPA DE LEITURA DO ARQUIVO (.CSV)")
+    print("========================================")
+
     caminho = encontrar_caminho_dados_csv(pasta_com_dados=nome_pasta_com_arquivo_csv, nome_dados=nome_arquivo)
     try:
         dados = pd.read_csv(filepath_or_buffer=caminho)
 
         if len(dados) == 0:
-            print("O arquivo CSV passado parece ter colunas, porém não tem valores (dados)! Verifique o parâmetro que você passou para a função de leitura!")
+            print("Erro: O arquivo CSV passado parece ter colunas, porém não tem valores (dados)! Verifique o parâmetro que você passou para a função de leitura!")
 
     except pandas.errors.EmptyDataError:
-        print("O arquivo CSV passado não possui dados! Verifique o parâmetro que você passou para a função de leitura!")
+        print("Erro: O arquivo CSV passado não possui dados! Verifique o parâmetro que você passou para a função de leitura!")
     
     except ValueError:
-        print("\nO caminho passado ao método: pd.read_csv() não encontra um arquivo CSV! Revise o caminho passado!")
+        print("\nErro: O caminho passado ao método: pd.read_csv() não encontra um arquivo CSV! Revise o caminho passado!")
 
     else:
         print("\nA Leitura do CSV foi executada sem exeções!")
